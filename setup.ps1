abea5f0ac9fba136d5432b68397c47c3ebcc5875
Cayde01 <84088250+Cayde01@users.noreply.github.com>
2022-10-07 15:29:12 +0200

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -167,4 +167,4 @@ msiexec /i "C:\TOOLS2\winfsp.msi" /q
 Start-Process -FilePath "C:\TOTAL\TOTALCMD.exe"
 
 <# MOUNT VIPER GALLERIES #>
-Start-Process -FilePath "C:\rclone\TRANSFER_BABES.bat"
+Start-Process -FilePath "C:\rclone\__COPY-PASTA.bat"
