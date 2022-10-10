3c548b3f6ea6df634a924776f58dc6dba7e1cc2f
Cayde01 <84088250+Cayde01@users.noreply.github.com>
2022-10-10 16:55:38 +0200

    Add files via upload

---
+++ MAKES-github.bat
@@ -0,0 +1,15 @@
+md "C:\drive-k\"
+subst K: C:\drive-k\
+
+md "K:\DOWNLOADS\"
+md "K:\WWW\"
+md "K:\X\"
+
+md "K:\WORK\ACTIVE-JOB"
+md "K:\WORK\ACTIVE-SOCIAL"
+md "K:\WORK\ACTIVE-VIDEOS"
+md "K:\WORK\COVERS_RAW"
+md "K:\WORK\COVERS_DONE"
+md "K:\WORK\RAR-DONE"
+md "K:\WORK\UPLOAD
+md "K:\WORK\ZZZ_DELETE_ZZZ"
